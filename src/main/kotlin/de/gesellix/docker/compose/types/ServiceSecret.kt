8ed9fa5a53be8968afda45ddeb2993e5335c2156
Tobias Gesellchen <tobias@gesellix.de>
2019-04-07 20:58:57 +0200

    service secret files should get mode/permissions 0444 by default
    see https://docs.docker.com/compose/compose-file/#long-syntax-2

--- src/main/kotlin/de/gesellix/docker/compose/types/ServiceSecret.kt
+++ src/main/kotlin/de/gesellix/docker/compose/types/ServiceSecret.kt
@@ -3,8 +3,8 @@ package de.gesellix.docker.compose.types
 data class ServiceSecret(
 
         var source: String = "",
-        var target: String = "",
-        var uid: String = "",
-        var gid: String = "",
-        var mode: Int = 0
+        var target: String? = "",
+        var uid: String? = "",
+        var gid: String? = "",
+        var mode: Int = 0b0100100100 // = from octal 0444
 )
