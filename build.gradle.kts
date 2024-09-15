15fabcd3767cda9eedd7a0339b3de951acb40cf2
Tobias Gesellchen <tobias@gesellix.de>
2024-09-15 15:01:49 +0200

    Bump dependency constraints

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
       implementation(it) {
         version {
           strictly("[1.6,3)")
-          prefer("2.0.10")
+          prefer("2.0.20")
         }
       }
     }
@@ -59,7 +59,7 @@ dependencies {
       implementation(it) {
         version {
           strictly("[3,4)")
-          prefer("3.9.0")
+          prefer("3.9.1")
         }
       }
     }
